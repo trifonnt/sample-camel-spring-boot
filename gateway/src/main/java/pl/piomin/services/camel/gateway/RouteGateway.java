1e4881aac5574fab9fab9138a60015e169add505
Trifon Trifonov <trifont@gmail.com>
2017-03-19 18:21:02 +0200

    Fixed ServiceCall to use dynamic HTTP_PATH

--- gateway/src/main/java/pl/piomin/services/camel/gateway/RouteGateway.java
+++ gateway/src/main/java/pl/piomin/services/camel/gateway/RouteGateway.java
@@ -1,6 +1,7 @@
 package pl.piomin.services.camel.gateway;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.remote.ConsulConfigurationDefinition;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -33,18 +34,37 @@ public class RouteGateway extends RouteBuilder {
 			.dataFormatProperty("prettyPrint", "true")
 			.port( port );
 
-		from("rest:get:account-new:/{accountId}").serviceCall("account-service", "http://account-service/account/${header.accountId}?bridgeEndpoint=true");
-		from("rest:get:account-new:/customer/{customerId}").serviceCall("account-service", "http://account-service/account/customer?bridgeEndpoint=true");
-		from("rest:post:account-new:/").serviceCall("account-service", "http://account-service/account?bridgeEndpoint=true");
-
 //		from("rest:get:hello/{me}").transform().simple("Bye ${header.me}"); // WORKS! http://localhost:8000/hello/trifon
 		from("rest:get:hello:/{me}").transform().simple("Hi ${header.me}"); // WORKS! http://localhost:8000/hello/trifon
 		from("rest:get:hello:/french/{me}").transform().simple("Bonjour ${header.me}"); // WORKS! http://localhost:8000/hello/french/trifon
 
 		// - http://localhost:8000/account-new WORKS!!!
-		from("rest:get:account-new").serviceCall("account-service", "http://account-service/account?connectTimeout=1000&bridgeEndpoint=true");
+		from("rest:get:account-new")
+			.serviceCall("account-service", "http://account-service/account?connectTimeout=1000&bridgeEndpoint=true");
 
 		// - http://localhost:8000/account-new/ WORKS!!!
-		from("rest:get:account-new:").serviceCall("account-service", "http://account-service/account?connectTimeout=1000&bridgeEndpoint=true");
+		from("rest:get:account-new:")
+			.serviceCall("account-service", "http://account-service/account?connectTimeout=1000&bridgeEndpoint=true");
+
+		// WORKS!!!
+		//   HTTP Component
+		// + http://camel.apache.org/http
+		//   serviceCall with exchangeProperty in URI 
+		// + http://camel.465427.n5.nabble.com/serviceCall-with-exchangeProperty-in-URI-td5788921.html
+//		from("rest:get:account-new:{accountId}") // works!!!
+//		from("rest:get:account-new/{accountId}") // works!!!
+		from("rest:get:account-new:/{accountId}")
+			//.setHeader(Exchange.HTTP_QUERY, simple("${exchangeProperty.accountId}"))
+			.setHeader(Exchange.HTTP_PATH, simple("account/${header.accountId}"))
+			.serviceCall("account-service", "http://account-service?bridgeEndpoint=true");
+
+		// WORKS!!!
+		from("rest:get:account-new:/customer/{customerId}")
+			.setHeader(Exchange.HTTP_PATH, simple("account/customer/${header.customerId}"))
+			.serviceCall("account-service", "http://account-service?bridgeEndpoint=true");
+
+		from("rest:post:account-new:/")
+			.setHeader(Exchange.HTTP_PATH, simple("account"))
+			.serviceCall("account-service", "http://account-service?bridgeEndpoint=true");
 	}
 }
